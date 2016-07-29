fa61b0dc7ec123c1b392cea815a5ef5808e130f3
Blake Strickland <Stricklandb02@gmail.com>
2016-07-28 20:11:17 -0400

    Create classes and call from different file

---
+++ Example.java
@@ -0,0 +1,9 @@
+public class Example {
+
+    public static void main(String[] args) {
+        // Your amazing code goes here...
+        System.out.println("NEW PEZ DISPENSER!!!!!!");
+        PezDispenser dispenser = new PezDispenser();
+        System.out.printf("The character is %s \n", dispenser.mCharacterName);
+    }
+}
