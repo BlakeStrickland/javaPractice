5faa69fc9571ab4796b1a88ab1338ab70b19e052
Blake Strickland <Stricklandb02@gmail.com>
2016-07-31 15:56:00 -0400

    Add comments

--- Example.java
+++ Example.java
@@ -1,11 +1,12 @@
+// Create class Example
 public class Example {
-
+  // declare main function that returns nothing and takes arbitrary arguments
     public static void main(String[] args) {
-        // Your amazing code goes here...
-
 
         System.out.println("NEW PEZ DISPENSER!!!!!!");
+        //Create new PezDispenser object
         PezDispenser dispenser = new PezDispenser("Leoardo");
+        //Call name method on new object
         System.out.printf("The character is %s \n", dispenser.getCharacterName());
     }
 }
--- PezDispenser.java
+++ PezDispenser.java
@@ -1,10 +1,20 @@
 public class PezDispenser {
-  private String mCharacterName;
+// set an immutable integer variable
+  public final int MAX_PEZ = 12;
 
+  private String mCharacterName;
+// declare pez count
+  private int mPezCount;
+// decalre public class that takes in 1 argument
   public PezDispenser(String characterName) {
     mCharacterName = characterName;
+    mPezCount = 0;
   }
-
+// decalre a load method that returns nothing but sets count
+  public void load() {
+    mPezCount = MAX_PEZ;
+  }
+  // declare public method that returns a string object of the charater name
   public String getCharacterName() {
     return mCharacterName;
   }
